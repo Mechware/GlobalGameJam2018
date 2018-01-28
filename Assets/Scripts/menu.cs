37f412a186be2c1e62f5850cd2d4986125a4dc4a
Gregory Perry <gregoryperry627@gmail.com>
2018-01-27 21:21:41 -0800

    desert map in menu

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour {
+    public GameObject Play;
+    public GameObject Forest;
+    public GameObject Desert;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +18,16 @@ public class menu : MonoBehaviour {
 	void Update () {
 		
 	}
-
+    public void chooseMap() {
+        Play.SetActive(false);
+        Forest.SetActive(true);
+        Desert.SetActive(true);
+    }
     public void startGameForest() {
         SceneManager.LoadScene("The Actual Scene");
+    }
+    public void startGameDesert() {
+        SceneManager.LoadScene("Maddie's Scene");
     }
 }
 
