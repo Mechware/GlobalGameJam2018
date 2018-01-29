a3e45f38b804c3402ff82e408b3b8a4ff11d98de
twistedburger <twistedburger@hotmail.com>
2018-01-28 17:28:51 -0800

    trying to make a server dude to control hits

---
+++ Assets/Scripts/ServerDude.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ServerDude : MonoBehaviour {
+
+    public Obelisk ob;
+
+    public void setOb(Obelisk ob) {
+        this.ob = ob;
+    }
+
+    public void respwnThem() {
+
+    }
+
+
+
+}
